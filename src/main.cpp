ecc0ff63981f86855befb7e51249c72c94ad096b
Stuart Brown <allspamignored@gmail.com>
2023-10-07 14:13:03 +1300

    Remove toggling of inbuilt LED in interrupt handler, was added for debugging and not needed anymore

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 // 0.96" OLED - connected via I2C
 // 2.2"  TFT - connected via SPI
 #define USE_OLED 0
-#define USE_TFT  1
+#define USE_TFT 1
 
 #if USE_OLED
 #include "oled.h"
@@ -55,13 +55,13 @@ double curtain_1_travel_time_ms = 0.0;
 double curtain_2_travel_time_ms = 0.0;
 
 // display update delay
-#define DISPLAY_UPDATE_TRIGGER (UINT16_MAX/2)
+#define DISPLAY_UPDATE_TRIGGER (UINT16_MAX / 2)
 uint32_t display_update_counter = DISPLAY_UPDATE_TRIGGER;
 
 //---------------------------------------------------
 // Detector 1 interrupt handler
 //---------------------------------------------------
-void detector1(void) 
+void detector1(void)
 {
   if (digitalRead(LASER_RECEIVER_1_INPUT))
   {
@@ -80,14 +80,12 @@ void detector1(void)
 //---------------------------------------------------
 // Detector 2 interrupt handler
 //---------------------------------------------------
-void detector2(void) 
+void detector2(void)
 {
-  int d2 = digitalRead(LASER_RECEIVER_2_INPUT);
-  digitalWrite(LED_BUILTIN, d2 ^ 1);
-  if (d2)
+  if (digitalRead(LASER_RECEIVER_2_INPUT))
   {
     ts_2_end_us = micros();
-    speed_2_measured = true;   
+    speed_2_measured = true;
   }
   else
   {
@@ -99,7 +97,7 @@ void detector2(void)
 //---------------------------------------------------
 // Detector 3 interrupt handler
 //---------------------------------------------------
-void detector3(void) 
+void detector3(void)
 {
   if (digitalRead(LASER_RECEIVER_3_INPUT))
   {
@@ -120,44 +118,44 @@ void detector3(void)
 //---------------------------------------------------
 void setup()
 {
-  #if DEBUG
+#if DEBUG
   Serial.begin(115200);
   Serial.flush();
-  #endif
-  
+#endif
+
   pinMode(LASER_RECEIVER_1_INPUT, INPUT);
   pinMode(LASER_RECEIVER_2_INPUT, INPUT);
   pinMode(LASER_RECEIVER_3_INPUT, INPUT);
-  
+
   pinMode(LASER_DIODE_1_OUTPUT, OUTPUT);
   pinMode(LASER_DIODE_2_OUTPUT, OUTPUT);
   pinMode(LASER_DIODE_3_OUTPUT, OUTPUT);
 
-  #if USE_OLED
-  #if DEBUG
+#if USE_OLED
+#if DEBUG
   Serial.println("Setup OLED");
-  #endif
+#endif
   oled_setup();
-  #endif
-  
-  #if USE_TFT
-  #if DEBUG 
-  Serial.println("Setup TFT");
-  #endif
-  tft_setup();
-  #endif
+#endif
 
-  // setup interrupts for the laser receivers
-  #if DEBUG 
+#if USE_TFT
+#if DEBUG
+  Serial.println("Setup TFT");
+#endif
+  tft_setup();
+#endif
+
+// setup interrupts for the laser receivers
+#if DEBUG
   Serial.println("Installing interrupt handlers");
-  #endif
+#endif
   attachPCINT(digitalPinToPCINT(LASER_RECEIVER_1_INPUT), detector1, CHANGE);
   attachPCINT(digitalPinToPCINT(LASER_RECEIVER_2_INPUT), detector2, CHANGE);
   attachPCINT(digitalPinToPCINT(LASER_RECEIVER_3_INPUT), detector3, CHANGE);
 
-  #if DEBUG
+#if DEBUG
   Serial.println("Ready... turning on lasers!");
-  #endif
+#endif
   digitalWrite(LASER_DIODE_1_OUTPUT, HIGH);
   digitalWrite(LASER_DIODE_2_OUTPUT, HIGH);
   digitalWrite(LASER_DIODE_3_OUTPUT, HIGH);
@@ -172,7 +170,7 @@ void loop()
 
   // Calculate shutter speeds independently so that a single sensor can
   // be used to measure only shutter speed or all 3 to measure shutter
-  // speed and curtain travel with out needing a mode change switch 
+  // speed and curtain travel with out needing a mode change switch
   //
   //           S3      S2      S1
   //            +       +       +
@@ -221,7 +219,7 @@ void loop()
     double shutter_speed_us = (double)(ts_1_end_us - ts_1_start_us);
     shutter_speed_1_ms = shutter_speed_us / 1000.0;
     fractional_shutter_speed_1 = 1000000.0 / shutter_speed_us;
-    #if DEBUG
+#if DEBUG
     Serial.print("t1_s=");
     Serial.print(ts_1_start_us);
     Serial.print("us   t1_e=");
@@ -232,16 +230,16 @@ void loop()
     Serial.println(" ms");
     Serial.print("Fractional Speed 1 = 1/");
     Serial.println(fractional_shutter_speed_1, 1);
-	  #endif
+#endif
   }
   if (speed_2_measured)
   {
-    speed_2_measured = false; 
+    speed_2_measured = false;
     display_update_counter = 0;
     double shutter_speed_us = (double)(ts_2_end_us - ts_2_start_us);
     shutter_speed_2_ms = shutter_speed_us / 1000.0;
     fractional_shutter_speed_2 = 1000000.0 / shutter_speed_us;
-    #if DEBUG
+#if DEBUG
     Serial.print("t2_s=");
     Serial.print(ts_2_start_us);
     Serial.print("us   t2_e=");
@@ -253,7 +251,7 @@ void loop()
     Serial.print("Fractional Speed 2 = 1/");
     Serial.println(fractional_shutter_speed_2, 1);
     Serial.println("");
-    #endif
+#endif
   }
   if (speed_3_measured)
   {
@@ -262,7 +260,7 @@ void loop()
     double shutter_speed_us = (double)(ts_3_end_us - ts_3_start_us);
     shutter_speed_3_ms = shutter_speed_us / 1000.0;
     fractional_shutter_speed_3 = 1000000.0 / shutter_speed_us;
-	  #if DEBUG
+#if DEBUG
     Serial.print("t3_s=");
     Serial.print(ts_3_start_us);
     Serial.print("us   t3_e=");
@@ -274,71 +272,71 @@ void loop()
     Serial.print("Fractional Speed 3 = 1/");
     Serial.println(fractional_shutter_speed_3, 1);
     Serial.println("");
-	  #endif
+#endif
   }
   if (curtain_1_measured && curtain_2_measured)
   {
     curtain_1_measured = false;
-    curtain_2_measured = false;    
+    curtain_2_measured = false;
     display_update_counter = 0;
     // check which is bigger and calculate accordingly
     // so that travel direction does not matter
     if (ts_3_start_us > ts_1_start_us)
     {
-      curtain_1_travel_time_ms = (double)(ts_3_start_us - ts_1_start_us)/1000.0;
+      curtain_1_travel_time_ms = (double)(ts_3_start_us - ts_1_start_us) / 1000.0;
     }
     else
     {
-      curtain_1_travel_time_ms = (double)(ts_1_start_us - ts_3_start_us)/1000.0;
+      curtain_1_travel_time_ms = (double)(ts_1_start_us - ts_3_start_us) / 1000.0;
     }
     if (ts_3_end_us > ts_1_end_us)
     {
-      curtain_2_travel_time_ms = (double)(ts_3_end_us - ts_1_end_us)/1000.0;
+      curtain_2_travel_time_ms = (double)(ts_3_end_us - ts_1_end_us) / 1000.0;
     }
     else
     {
-      curtain_2_travel_time_ms = (double)(ts_1_end_us - ts_3_end_us)/1000.0;
+      curtain_2_travel_time_ms = (double)(ts_1_end_us - ts_3_end_us) / 1000.0;
     }
 
-    #if DEBUG
+#if DEBUG
     Serial.print("Curtain 1 travel time=");
-    Serial.print(curtain_1_travel_time_ms,1);
+    Serial.print(curtain_1_travel_time_ms, 1);
     Serial.println(" ms");
 
     Serial.print("Curtain 2 travel time=");
-    Serial.print(curtain_2_travel_time_ms,1);
+    Serial.print(curtain_2_travel_time_ms, 1);
     Serial.println(" ms");
- 
+
     Serial.println("");
-    #endif
+#endif
   }
 
   // --------- display ---------
-  if ( display_update_counter == DISPLAY_UPDATE_TRIGGER )
+  if (display_update_counter == DISPLAY_UPDATE_TRIGGER)
   {
-    #if USE_OLED
-    oled_show_values( shutter_speed_1_ms, 
-                      shutter_speed_2_ms,
-                      shutter_speed_3_ms,
-                      fractional_shutter_speed_1, 
-                      fractional_shutter_speed_2,
-                      fractional_shutter_speed_3,
-                      curtain_1_travel_time_ms,
-                      curtain_2_travel_time_ms );
-    #endif
-    
-    #if USE_TFT
-    tft_show_values( shutter_speed_1_ms, 
+#if USE_OLED
+    oled_show_values(shutter_speed_1_ms,
                      shutter_speed_2_ms,
                      shutter_speed_3_ms,
-                     fractional_shutter_speed_1, 
+                     fractional_shutter_speed_1,
                      fractional_shutter_speed_2,
                      fractional_shutter_speed_3,
                      curtain_1_travel_time_ms,
-                     curtain_2_travel_time_ms );
-    #endif
+                     curtain_2_travel_time_ms);
+#endif
+
+#if USE_TFT
+    tft_show_values(shutter_speed_1_ms,
+                    shutter_speed_2_ms,
+                    shutter_speed_3_ms,
+                    fractional_shutter_speed_1,
+                    fractional_shutter_speed_2,
+                    fractional_shutter_speed_3,
+                    curtain_1_travel_time_ms,
+                    curtain_2_travel_time_ms);
+#endif
   }
-  if ( display_update_counter <= DISPLAY_UPDATE_TRIGGER )
+  if (display_update_counter <= DISPLAY_UPDATE_TRIGGER)
   {
     display_update_counter++;
   }
